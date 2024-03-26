688fdc36edf8ddc711ecf957a1c6f9fe3630b0f3
RafaelMonteiro82 <97414484+RafaelMonteiro82@users.noreply.github.com>
2024-03-26 03:11:17 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,15 @@
 function Home() {
- return<h1>Juliane eu amo você. Se você me ama dá uma risadinha! amo você</h1>
+  return (
+    <h1>Juliane eu amo você. Se você me ama dá uma risadinha! amo você</h1>
+  );
+}
 
+function Home() {
+  console.log("teste");
+}
+
+function teste() {
+  console.log("indentação");
 }
 
 export default Home;
